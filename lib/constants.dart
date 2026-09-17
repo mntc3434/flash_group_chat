059951128a1415caf9f370c22b3e3845b4ea471c
Mintesinot getu <148215622+mntc3434@users.noreply.github.com>
2026-09-17 22:39:26 +0300

    Update message text field decoration constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -5,7 +5,7 @@ const kSendButtonTextStyle = TextStyle(
   fontWeight: FontWeight.bold,
   fontSize: 18.0,
 );
-.....
+....
 const kMessageTextFieldDecoration = InputDecoration(
   contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
   hintText: 'Type your message here...',
